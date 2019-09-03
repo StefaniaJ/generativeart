40460f327116e395c74c28c08e45559a92fdffce
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-03 15:32:30 +0200

    created the variable for artworks

---
+++ art.js
@@ -0,0 +1,15 @@
+"use strict";
+const artWork1 = document.querySelector("#artwork1");
+const artWork2 = document.querySelector("#artwork2");
+const artWork3 = document.querySelector("#artwork3");
+const artWork4 = document.querySelector("#artwork4");
+const artWork5 = document.querySelector("#artwork5");
+const artWork6 = document.querySelector("#artwork6");
+const artWork7 = document.querySelector("#artwork7");
+const artWork8 = document.querySelector("#artwork8");
+const artWork9 = document.querySelector("#artwork9");
+
+//Artwork 1
+// for (let counter = 0; counter < 10; counter++) {
+//   console.log("From 0 to 9: " + counter);
+// }
