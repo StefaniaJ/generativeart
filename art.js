7514280ddfda6103b2d3bcc684d77320dedaa405
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-03 16:02:39 +0200

    artwork3 done

--- art.js
+++ art.js
@@ -14,10 +14,10 @@ for (let i = 100; i < 300; i += 20) {
   //create a new div
   let newDiv = document.createElement("div");
 
-  //added a specific class  for div
+  //add a right  class  for div
   newDiv.classList.add("box");
 
-  //set the height and the width
+  //set the height and width
   newDiv.style.height = i + "px";
   newDiv.style.width = i + "px";
 
@@ -30,12 +30,28 @@ for (let i = 0; i < 90; i += 10) {
   //create a new div
   let newDiv = document.createElement("div");
 
-  //add the specific class
+  //add the right class
   newDiv.classList.add("box");
 
-  //change hei,width...
+  //add some style
   newDiv.style.transform = `rotate(${i}deg)`;
 
   //append child
   artWork2.appendChild(newDiv);
+}
+
+// Artwork 3
+for (let i = 0; i < 200; i += 10) {
+  //make a new div
+  let newDiv = document.createElement("div");
+
+  // add a right class for div
+  newDiv.classList.add("circle");
+
+  //add some style
+  newDiv.style.height = i + "px";
+  newDiv.style.width = i + "px";
+
+  //append the child (div) at artwork3
+  artWork3.appendChild(newDiv);
 }
